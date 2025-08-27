758bd977ab2f3fe7a92385ec678faab9ec8d0a72
ahmed-tasin <tasinahmedta910976@gmail.com>
2025-08-27 22:18:26 +0600

    text filter function

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const input = document.getElementById("todoInput");
 const list = document.getElementById("todoList");
 
 
-
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const text = input.value.trim();
+    if (text !== "") {
+    addTodo(text);
+    input.value = "";
+}
+});
 
 
 
