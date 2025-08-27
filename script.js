ed9500f418f380468e2fb968811beb4d46147f02
ahmed-tasin <tasinahmedta910976@gmail.com>
2025-08-27 22:23:15 +0600

    edit done btn

--- script.js
+++ script.js
@@ -13,6 +13,24 @@ todoForm.addEventListener("submit", (e) => {
 });
 
 
+function addTodo(text) {
+    const li = document.createElement("li");
+    li.className = "flex justify-between items-center bg-white p-3 rounded";
+
+    const span = document.createElement("span");
+    span.textContent = text;
+    span.className = "flex-grow text-black";
+
+
+    const doneBtn = document.createElement("button");
+    doneBtn.innerHTML = "✔️";
+    doneBtn.className =
+    "bg-red-600 text-white px-3 py-1 rounded hover:bg-red-700 mr-2";
+    doneBtn.onclick = () => {
+    span.classList.toggle("line-through");
+    span.classList.toggle("opacity-50");
+};
+
 
 
 
